30fe2e26067e7dc54fa51fc2ab901f55f920726f
Polska Panda <cookavsky@gmail.com>
2019-01-19 22:31:28 +0100

    C

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Anom' },
+    { id: 12, name: 'Anomiotator' },
+    { id: 13, name: 'Kuka' },
+    { id: 14, name: 'Cook' },
+    { id: 15, name: 'Nimi' },
+    { id: 16, name: 'Mura' },
+    { id: 17, name: 'Roshi' },
+    { id: 18, name: 'Nimiroshi' },
+    { id: 19, name: 'Romm' },
+    { id: 20, name: 'Zuzu' },
+];
